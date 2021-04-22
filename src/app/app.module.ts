c625ca7ab350216dd91d00bb1fbf9cdd01b00e05
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-04-22 03:37:32 +0300

    First commit
    Eeeee, Angular)))

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { AsideLeftComponent } from './aside-left/aside-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    BannerComponent,
+    MainContainerComponent,
+    AsideLeftComponent
   ],
   imports: [
     BrowserModule
