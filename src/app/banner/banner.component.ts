4e6e64937f691e40e77477482583e7d3f351a86c
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-05-04 02:39:28 +0300

    ChangeDetection

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-banner",
   templateUrl: "./banner.component.html",
-  styleUrls: ["./banner.component.css"]
+  styleUrls: ["./banner.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BannerComponent implements OnInit {
 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
-  styleUrls: ["./main.component.css"]
+  styleUrls: ["./main.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent implements OnInit {
 
